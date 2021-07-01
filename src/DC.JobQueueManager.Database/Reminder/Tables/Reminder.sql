dcebf8d4b676a65fd39e0db1431eb3129203ecc4
russellball63edu <82030012+russellball63edu@users.noreply.github.com>
2021-07-01 17:13:56 +0100

    Add files via upload

---
+++ src/DC.JobQueueManager.Database/Reminder/Tables/Reminder.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Reminder].[Reminder]
+(
+	[ReminderId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [ReminderDate] DATE NULL, 
+    [DeadlineDate] DATE NOT NULL, 
+    [ClosedDate] DATE NULL, 
+    [Notes] NVARCHAR(MAX) NULL, 
+    [CreatedOn] DATETIME NOT NULL, 
+    [UpdatedOn] DATETIME NULL, 
+    [CreatedBy] NVARCHAR(250) NOT NULL, 
+    [UpdatedBy] NVARCHAR(250) NULL
+)
---
+++ src/DC.JobQueueManager.Database/Reminder/Tables/ReminderCertificate.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Reminder].[ReminderCertificate]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ReminderId] INT NOT NULL, 
+    [Name] NVARCHAR(250) NOT NULL, 
+    [Thumbprint] NVARCHAR(250) NOT NULL, 
+    CONSTRAINT [FK_ReminderCertificate_Reminder] FOREIGN KEY ([ReminderId]) REFERENCES [Reminder].[Reminder]([ReminderId])
+)
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_ReminderCertificate_ReminderId] ON [Reminder].[ReminderCertificate] ([ReminderId])
+GO
