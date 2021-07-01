d6e0a5c31f33589159fa52ce0d9009014dedd961
russellball63edu <82030012+russellball63edu@users.noreply.github.com>
2021-07-01 17:14:36 +0100

    Add files via upload

---
+++ src/DC.JobQueueManager.Database/ServiceMessage/ServicePageMessage.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [ServiceMessage].[ServicePageMessage] (
+	[PageId]				INT					NOT NULL,
+	[MessageId]				INT					NOT NULL
+	
+
+    CONSTRAINT [PK_ServicePageMessage] PRIMARY KEY CLUSTERED ( [PageId] ASC, [MessageId] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, DATA_COMPRESSION = PAGE)
+
+	CONSTRAINT [FK_ServicePageMessage_ServicePage] FOREIGN KEY ([PageId]) REFERENCES [ServiceMessage].[ServicePage] ([Id])
+	CONSTRAINT [FK_ServicePageMessage_ServiceMessage] FOREIGN KEY ([MessageId]) REFERENCES [ServiceMessage].[ServiceMessage] ([Id])
+)
+
+GO
