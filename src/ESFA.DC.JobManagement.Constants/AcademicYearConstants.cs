0574231a1ca86eae4f3740bc80aa4eddfe63e2b6
russellball63edu <82030012+russellball63edu@users.noreply.github.com>
2021-07-01 16:48:03 +0100

    Add files via upload

---
+++ src/ESFA.DC.JobManagement.Constants/AcademicYearConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace ESFA.DC.JobManagement.Constants
+{
+    public class AcademicYearConstants
+    {
+        public const int Y1819 = 1819;
+        public const int Y1920 = 1920;
+        public const int Y2021 = 2021;
+        public const int Y2122 = 2122;
+    }
+}
---
+++ src/ESFA.DC.JobManagement.Constants/JobStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace ESFA.DC.JobManagement.Constants
+{
+    public class JobStatus
+    {
+        public const int Ready = 1;
+        public const int MovedForProcessing = 2;
+        public const int Processing = 3;
+        public const int Completed = 4;
+        public const int FailedRetry = 5;
+        public const int Failed = 6;
+        public const int Paused = 7;
+        public const int Waiting = 8;
+    }
+}
