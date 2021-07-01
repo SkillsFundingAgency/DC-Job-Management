0858f7ee8c085c128a5637115e1c45e6323ae49e
russellball63edu <82030012+russellball63edu@users.noreply.github.com>
2021-07-01 16:47:40 +0100

    Add files via upload

---
+++ src/ESFA.DC.JobManagement.Configuration/FundingClaimsDbConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ESFA.DC.JobManagement.Configuration
+{
+    public class FundingClaimsDbConfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
+}
