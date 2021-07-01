f02fb4f9c79d964cf41f10810ae3d032b4a4551e
russellball63edu <82030012+russellball63edu@users.noreply.github.com>
2021-07-01 17:00:24 +0100

    Add files via upload

---
+++ src/ESFA.DC.PeriodEnd.Service.Clients/ClientService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace ESFA.DC.PeriodEnd.Service.Clients
+{
+    public class ClientService
+    {
+        public async Task<TResult> PostAsync<TContent, TResult>(string baseUrl, string endPoint, TContent content)
+        {
+            return await baseUrl
+                .AppendPathSegment(endPoint)
+                .PostJsonAsync(content)
+                .ReceiveJson<TResult>();
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string baseUrl, string endPoint, IDictionary<string, object> parameters)
+        {
+            var clientUrl = baseUrl.AppendPathSegment(endPoint);
+
+            foreach (var parameter in parameters)
+            {
+                clientUrl.SetQueryParam(parameter.Key, parameter.Value);
+            }
+
+            return await clientUrl.GetJsonAsync<TResult>();
+        }
+    }
+}
---
+++ src/ESFA.DC.PeriodEnd.Service.Clients/DASClientService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ESFA.DC.PeriodEnd.Interfaces;
+using ESFA.DC.PeriodEnd.Models;
+using ESFA.DC.PeriodEnd.Models.Config;
+
+namespace ESFA.DC.PeriodEnd.Service.Clients
+{
+    public class DASClientService : ClientService, IDASClientService
+    {
+        private const string DASSubmissionEndPoint = "Submission/Successful";
+        private const string AcademicYearParameter = "academicYear";
+        private const string CollectionPeriodParameter = "collectionPeriod";
+        private const string AuthenticationParameter = "code";
+
+        private readonly DASApiSettings _apiSettings;
+
+        public DASClientService(DASApiSettings apiSettings)
+        {
+            _apiSettings = apiSettings;
+        }
+
+        public async Task<IEnumerable<DASSubmission>> GetDASProcessedProviders(int academicYear, int period)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                { AuthenticationParameter, _apiSettings.Authentication },
+                { AcademicYearParameter, academicYear },
+                { CollectionPeriodParameter, period }
+            };
+
+            var result = await GetAsync<DASJobSubmissions>(_apiSettings.BaseUrl, DASSubmissionEndPoint, parameters);
+
+            return result?.SuccessfulSubmissionJobs;
+        }
+    }
+}
